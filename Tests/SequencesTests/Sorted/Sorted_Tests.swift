574f8f1d9798e054be9158e39bbd46ba83fe34ab
Alexandre <alexandresaad@icloud.com>
2021-10-28 22:04:51 +0200

    Commit

---
+++ Tests/SequencesTests/Sorted/Sorted_Tests.swift
@@ -0,0 +1,20 @@
+// Sorted_Tests.swift
+// SequencesTests
+//
+// Copyright © 2021 Alexandre H. Saad
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+
+import XCTest
+@testable import Sequences
+
+final class Sorted_Tests: XCTestCase {
+	@Sorted(by: <) fileprivate var values: [Int] = [1, 3, 2]
+	
+	func test_clamped_succeeds() {
+		XCTAssertEqual(self.values, [1, 2, 3])
+		
+		self.values.append(0)
+		XCTAssertEqual(self.values, [0, 1, 2, 3])
+	}
+}
