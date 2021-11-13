03a265c5208425e76c4ca63c0a70ae925dd15c69
Alexandre <alexandresaad@icloud.com>
2021-11-13 21:40:52 +0100

    Commit

---
+++ Sources/Extensions/Methods/Fibonacci.swift
@@ -0,0 +1,30 @@
+// Fibonacci.swift
+// Extensions
+//
+// Copyright © 2021 Alexandre H. Saad
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+
+/// Returns fibonnaci numbers starting from zero.
+///
+/// ```swift
+/// print(fibonacci(9))
+/// // Prints "[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]"
+/// ```
+///
+/// - Parameter numbers: The number of numbers.
+/// - Returns: The fibonacci sequence.
+public func fibonacci<Value>(_ count: Array.Index) -> [Value]
+where Value: Comparable & Numeric & Strideable, Value.Stride: SignedInteger {
+	guard count > 1 else {
+		return [.zero]
+	}
+	
+	var sequence: [Value] = [0, 1]
+
+	for index in 2...count {
+		sequence.append(sequence[index - 1] + sequence[index - 2])
+	}
+	
+	return sequence
+}
---
+++ Tests/ExtensionsTests/Int/Int_Tests.swift
@@ -0,0 +1,19 @@
+// Int_Tests.swift
+// ExtensionsTests
+//
+// Copyright © 2021 Alexandre H. Saad
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+
+import XCTest
+@testable import Arithmetics
+
+final class Int_Tests: XCTestCase {
+	func test_fibonacci_Succeeds() {
+		// Given
+		let sequence: [Int] = fibonacci(9)
+		
+		// Then
+		XCTAssertEqual(sequence, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+	}
+}
